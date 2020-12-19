9d496a921b1e74bbb6fd0ead10b03ff4ce4240f3
vikhyat0310 <61613086+vikhyat0310@users.noreply.github.com>
2020-12-20 00:47:39 +0530

    Add files via upload
    added projects section

---
+++ js/projects.js
@@ -0,0 +1,11 @@
+$(function(){
+  $slideshow = $(".gallery ul");
+  $slideactive = $slideshow.find("li.each").first().addClass('active').show();
+  $(".direction .next").click(function(){
+  $slideactive = $slideshow.find("li.active").next();
+    if(!$slideactive.length)
+      $slideactive = $slideshow.find("li.each").first();   
+      $slideshow.find("li.active").removeClass("active");
+      $slideactive.addClass("active");
+  });
+});
