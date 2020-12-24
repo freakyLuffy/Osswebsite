f0d4478f703ecd00affcbb68006b356e9dbf6988
Himanshu <hk062848@gmail.com>
2020-12-24 20:50:27 +0530

    backend and frontend
    Co-Authored-By: vikhyat0310 <61613086+vikhyat0310@users.noreply.github.com>
    Co-Authored-By: shiviii123456 <70109170+shiviii123456@users.noreply.github.com>
    Co-Authored-By: Anushka Shukla <63242573+anushu24@users.noreply.github.com>

---
+++ js/component/navbar.js
@@ -0,0 +1,11 @@
+window.document.write(`
+    <nav class="navbar navbar-expand-lg static-top shadow bg-white sticky-top" data-aos="fade-in">
+        <div class="container">
+            <a class="navbar-brand d-flex align-items-center" href="/">
+                <img src="./img/OSS-LOGO.jpeg" height="40" width="auto">
+                <span class="ml-3 text-dark">Team OSS</span>
+            </a>
+            
+        </div>
+    </nav>
+`);
